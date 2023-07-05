5459538757550c1b4a6324a2fa4001a6ab94e3e7
ashish Bindra <ashishbindra2@gmail.com>
2023-07-05 18:06:39 +0530

    working on it

--- fastapi-pymysql/main.py
+++ fastapi-pymysql/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request, Response
+
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -10,6 +11,15 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
 
 # Dependency
 def get_db():
